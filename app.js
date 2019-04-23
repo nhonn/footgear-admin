cf4862f9b08dd1d56f90f4eb19a34db340c28ec6
Nhon Nguyen <nhonnt97@gmail.com>
2019-04-23 21:17:38 +0700

    add manage account view

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
@@ -6,7 +5,6 @@ const logger = require("morgan");
 const sassMiddleware = require("node-sass-middleware");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 
 const app = express();
 
@@ -33,15 +31,14 @@ app.use(
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function(req, res) {
+  res.render("error404");
 });
 
 // error handler
-app.use(function (err, req, res) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/accounts", function(req, res) {
+  res.render("accounts", { title: "Quản lý tài khoản" });
+});
+
+router.get("/account-detail", (req, res) => {
+  res.render("accountDetail", { title: "Thông tin tài khoản" });
 });
 
 module.exports = router;
